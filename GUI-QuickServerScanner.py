13e5c06e71015a0dd5d7e52e85ea2aa976269cfb
Gerardo Barcenas <gbarcenasjr@gmail.com>
2024-08-01 11:13:48 -0700

    Started a GUI Version of the App

---
+++ GUI-QuickServerScanner.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from datetime import datetime
+
+root = Tk()
+root.title("Quick Server Scanner")
+root.minsize(200, 200)  # width, height
+root.geometry("800x800+50+50")  # width x height + x + y
+
+TitleLable = Label(root, text = "Are you a Geek?")
+
+
+root.mainloop()
