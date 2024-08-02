5a7d59a61ad2688157ec6aa34df43d7d9f27d6b6
Gerardo Barcenas <gbarcenasjr@gmail.com>
2024-08-02 07:52:17 -0700

    Reformated GUI Code to PEP8 Standard

--- GUI-QuickServerScanner.py
+++ GUI-QuickServerScanner.py
@@ -1,12 +1,46 @@
-from tkinter import *
-from datetime import datetime
+import tkinter as tk
 
-root = Tk()
+# from datetime import datetime
+
+# root window initialization
+root = tk.Tk()
 root.title("Quick Server Scanner")
-root.minsize(200, 200)  # width, height
-root.geometry("800x800+50+50")  # width x height + x + y
+root.geometry('720x700')
 
-TitleLable = Label(root, text = "Are you a Geek?")
+list_counter = 0
 
 
+# Functions
+def onclick():
+    print("You clicked the button")
+
+
+def getCounter() -> int:
+    return list_counter
+
+
+def incrementCounter(counter):
+    counter += 1
+
+
+def userPressedEnter(event):
+    example.insert(0, txt.get())
+
+
+# Main Body
+# adding a label to the root window
+lbl = tk.Label(root, text="Enter the SN for the Search List: ")
+lbl.grid(column=0, row=0)
+
+# adding Entry Field
+txt = tk.Entry(root, width=80)
+txt.bind('<Return>', userPressedEnter)
+txt.grid(column=1, row=0)
+
+# Set ListBox
+example = tk.Listbox(root, width=100, height=30)
+example.grid(column=0, row=1, columnspan=3)
+example.insert(0, "")
+
+# Execute Tkinter
 root.mainloop()
